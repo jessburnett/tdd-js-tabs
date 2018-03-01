1d9cac29aad91faf54a998e867481d1742056122
jessburnett <jess.a.burnett@gmail.com>
2018-03-01 11:46:15 -0500

    implement jake for automated build tool and set default task

--- Jakefile.js
+++ Jakefile.js
@@ -1,11 +1,17 @@
 //Copyright 2018 Jessica Burnett. All rights reserved.
 // builds should 1-self documenting 2-command-line process 3-dependency resolution
 // 4- code, not configuration 5-straightforward and simple (Jake, Grunt, Gulp)
-
+/* dependency management schools:
+* 1 - automatically install dependencies
+*     a - cons: dependency version may not be available.
+* 2 -
+* */
 //wrap in iffy to avoid namespace errors
 (function(){
     "use strict";
 
-    console.log("\n\nBUILD OK");
+    task("default", function(){
+        console.log("\n\nBUILD OK");
+    })
 
-}());
+}());//end iffy
