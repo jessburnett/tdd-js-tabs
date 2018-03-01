ab71b3f4f2007fd8c067781e7a65f56cb3d7f4c5
jessburnett <jess.a.burnett@gmail.com>
2018-03-01 09:22:30 -0500

    bare node automated build for project

---
+++ build.js
@@ -0,0 +1,9 @@
+//Copyright 2018 Jessica Burnett. All rights reserved.
+
+//wrap in iffy to avoid namespace errors
+(function(){
+    "use strict";
+
+    console.log("setup automated build at ground zero");
+
+}());
