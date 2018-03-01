6921f055ea4e686aed34a727d64f2d3e2163fdd8
jessburnett <jess.a.burnett@gmail.com>
2018-03-01 11:34:42 -0500

    install jake

--- build.js
+++ build.js
@@ -1,4 +1,6 @@
 //Copyright 2018 Jessica Burnett. All rights reserved.
+// builds should 1-self documenting 2-command-line process 3-dependency resolution
+// 4- code, not configuration 5-straightforward and simple (Jake, Grunt, Gulp)
 
 //wrap in iffy to avoid namespace errors
 (function(){
